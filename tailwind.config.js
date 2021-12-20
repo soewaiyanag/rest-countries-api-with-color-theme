382d600752b83f171dd27dabd5d342485df84c6f
soewaiyanag <soewaiyanag@gmail.com>
2021-12-20 18:09:40 +0630

    setup files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./src/*.html"],
+  darkMode: "class",
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "src",
+    watchFiles: ["src", "dist"],
+  },
+};
