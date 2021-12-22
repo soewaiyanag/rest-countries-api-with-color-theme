6738be030eb1281f1a932ec748b97efa3c259265
soewaiyanag <soewaiyanag@gmail.com>
2021-12-22 13:41:01 +0630

    basic grid layout to show  all countries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 import "./style.css";
+
+const main = document.querySelector("main");
+
+let countriesData = new Promise((resolve, reject) => {
+  let dataURL = "https://restcountries.com/v2/all";
+  fetch(dataURL)
+    .then((response) => response.json())
+    .then((data) => {
+      resolve(data);
+    });
+});
+
+countriesData.then((value) => {
+  value.forEach((country) => {
+    const box = document.createElement("div");
+    const flag = document.createElement("img");
+    const info = document.createElement("div");
+
+    box.className = "box";
+
+    flag.src = country.flags.png;
+    flag.className = "flag";
+
+    info.className = "info";
+    info.innerHTML = `
+        <h1 class="">${country.name}</h1>
+        <h2>
+        Population:
+            <span>${country.population || "unknow"}</span>
+        </h2>
+        <h2>
+        Region:
+            <span>${country.region || "unknow"}</span>
+        </h2>
+        <h2>
+        Capital:
+        <span>${country.capital || "unknow"}</span>
+        </h2>
+    `;
+    box.appendChild(flag);
+    box.appendChild(info);
+    main.appendChild(box);
+  });
+});
+
+// <div class="box">
+// <img src="https://flagcdn.com/mm.svg" alt="flag" />
+// <div class="box__text">
+//   <h1 class="">Myanmar</h1>
+//   <h2>
+//     Population:
+//     <span>100000000</span>
+//   </h2>
+//   <h2>
+//     Region:
+//     <span>Asia</span>
+//   </h2>
+//   <h2>
+//     Capital:
+//     <span>Nay Pyi Daw</span>
+//   </h2>
+// </div>
+// </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/*.html"],
   darkMode: "class",
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue-100": "hsl(209, 23%, 22%)",
+        "dark-blue-200": "hsl(207, 26%, 17%)",
+        "dark-blue-300": "hsl(200, 15%, 8%)",
+        "stone-450": "#858585",
+      },
+      fontFamily: {
+        nunito: "'Nunito Sans', 'sans-serif'",
+      },
+    },
   },
   plugins: [],
 };
